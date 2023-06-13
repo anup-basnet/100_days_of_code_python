79b94b8abbf6f9365d64781ac82aa5d65e0bec2c
Anup <anupbasnet057@gmail.com>
2023-06-13 23:21:15 +0545

    day1: print, strings, variables and input function

---
+++ day_1.py
@@ -0,0 +1,12 @@
+# Printing | Input Function | String Manipulation | Variables
+# project: band-name-generator-end
+
+print("Welcome to the Band Name Generator.")
+
+city = input("What's the name of the city you grew up in? \n")
+pet = input("What's your pet's name? \n")
+
+print("Your band name could be " + city + " " + pet)
+
+# You would not learn this in day 1 😂😂
+# print(f"Your band name could be {city} {pet}")
