b3bed1e3f60455fce52a6db2d4092ca6b6b0a9e8
Anup <anupbasnet057@gmail.com>
2023-06-16 13:52:11 +0545

    Project: Reeborgs World challenge
    Use Functions, while loops and conditionals to reach the final destination.

--- day_6/day_6.py
+++ day_6/day_6.py
@@ -9,7 +9,7 @@ Reeborg was exploring a dark maze and the battery in its flashlight ran out.
 http://reeborg.ca/reeborg.html?lang=en&mode=python&menu=%2Fworlds%2Fmenus%2Fselect_collection_en.json&name=Alone&url=%2Fworlds%2Ftutorial_en%2Falone.json
 - And import the json files by cliking on Additional Options and then Open world from file (3 json files for 3 scenarios)
 
-# Instructions
+# Instructionsi
 Write a program using an if/elif/else statement so Reeborg can find the exit. The secret is to have Reeborg follow along the right edge of the maze, turning right if it can, going straight ahead if it can't turn right, or turning left as a last resort.
 
 # What you need to know
