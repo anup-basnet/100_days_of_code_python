67234cd5235def9682511862e9f4689ee3af9016
Anup <anupbasnet057@gmail.com>
2023-06-25 14:57:25 +0545

    Project: snake-game-final
    - Snake Class: create snake and extend body on getting food.
    - Food Class: generate food at random screen position.
    - scoreboard Class: keep track of user score & display.

---
+++ day_21/main.py
@@ -0,0 +1,67 @@
+# Project: Snake-Game
+"""
+# Steps  [Upto 3 steps only for day_20 ]
+1. Create a snake body
+2. Move the snake
+3. Control the snake
+4. Detect collision with food
+5. Create a scoreboard
+6. Detect collision with the wall
+7. Detect collision with tail 
+"""
+
+from turtle import Screen
+from snake import Snake
+from food import Food
+from scoreboard import Scoreboard
+import time
+
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("My Snake Game")
+screen.tracer(0)
+
+snake = Snake()
+food = Food()
+scoreboard = Scoreboard()
+
+
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+    snake.move()
+
+    # Detect collision with the food
+    if snake.head.distance(food) < 15:
+        scoreboard.increase_score()
+        snake.extend()
+        food.refresh()
+
+    # Detect collision with the wall
+    if (
+        snake.head.xcor() > 280
+        or snake.head.xcor() < -280
+        or snake.head.ycor() > 280
+        or snake.head.ycor() < -280
+    ):
+        game_is_on = False
+        scoreboard.game_over()
+
+    # Detect collision with the tail
+    # segments[0] = head of snake -- body starts from index 1
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 10:
+            game_is_on = False
+            scoreboard.game_over()
+
+
+screen.exitonclick()
