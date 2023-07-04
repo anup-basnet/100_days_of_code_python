3bc7fdcfb4d2a67d3275e50ff194ba62957bebfc
Anup <anupbasnet057@gmail.com>
2023-07-04 23:34:21 +0545

    Error Handling for day_29 project

---
+++ day_30/password-manager-start/main.py
@@ -0,0 +1,165 @@
+# Exception Handling for day_29 project and writing to json file
+
+from tkinter import *
+from tkinter import messagebox
+from random import randint, choice, shuffle
+import json
+
+# import pyperclip
+
+
+# ---------------------------- PASSWORD GENERATOR ------------------------------- #
+def generate_password():
+    letters = [
+        "a",
+        "b",
+        "c",
+        "d",
+        "e",
+        "f",
+        "g",
+        "h",
+        "i",
+        "j",
+        "k",
+        "l",
+        "m",
+        "n",
+        "o",
+        "p",
+        "q",
+        "r",
+        "s",
+        "t",
+        "u",
+        "v",
+        "w",
+        "x",
+        "y",
+        "z",
+        "A",
+        "B",
+        "C",
+        "D",
+        "E",
+        "F",
+        "G",
+        "H",
+        "I",
+        "J",
+        "K",
+        "L",
+        "M",
+        "N",
+        "O",
+        "P",
+        "Q",
+        "R",
+        "S",
+        "T",
+        "U",
+        "V",
+        "W",
+        "X",
+        "Y",
+        "Z",
+    ]
+    numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+    symbols = ["!", "#", "$", "%", "&", "(", ")", "*", "+"]
+
+    password_letters = [choice(letters) for _ in range(randint(8, 10))]
+    password_symbols = [choice(symbols) for _ in range(randint(2, 4))]
+    password_numbers = [choice(numbers) for _ in range(randint(2, 4))]
+
+    password_list = password_letters + password_symbols + password_numbers
+    shuffle(password_list)
+
+    password = "".join(password_list)
+    password_entry.delete(0, END)
+    password_entry.insert(0, password)
+    # pyperclip.copy(password)
+
+
+# ---------------------------- SAVE PASSWORD ------------------------------- #
+def add_data():
+    # Get the data
+    website = website_entry.get()
+    email = email_entry.get()
+    password = password_entry.get()
+    new_data = {
+        website: {
+            "email": email,
+            "password": password,
+        }
+    }
+
+    # validation and save data
+    if not website or not password:
+        messagebox.showinfo(
+            title="Oops", message="Please don't leave any fields empty!"
+        )
+    else:
+        try:
+            with open("data.json", mode="r") as data_file:
+                data = json.load(data_file)
+
+        # create json file if there is not any and store the data
+        except FileNotFoundError:
+            with open("data.json", mode="w") as data_file:
+                json.dump(new_data, data_file, indent=4)
+        else:
+            data.update(new_data)
+            with open("data.json", mode="w") as data_file:
+                json.dump(data, data_file, indent=4)
+        finally:
+            # clear the widgets
+            website_entry.delete(0, END)
+            password_entry.delete(0, END)
+
+
+# ---------------------------- UI SETUP ------------------------------- #
+
+window = Tk()
+window.title("Password Manager")
+window.config(padx=50, pady=50)
+
+canvas = Canvas(width=200, height=200)
+logo = PhotoImage(file="logo.png")
+canvas.create_image(100, 100, image=logo)
+canvas.grid(row=0, column=1)
+
+
+# website
+website_label = Label(text="Website:")
+website_label.grid(row=1, column=0)
+
+website_entry = Entry(width=52)
+website_entry.focus()
+website_entry.grid(row=1, column=1, columnspan=2)
+
+
+# Email/username
+email_label = Label(text="Email/Username:")
+email_label.grid(row=2, column=0)
+
+email_entry = Entry(width=52)
+email_entry.insert(0, "email@gmail.com")
+email_entry.grid(row=2, column=1, columnspan=2)
+
+
+# Password
+password_label = Label(text="Password:")
+password_label.grid(row=3, column=0)
+
+password_entry = Entry(width=34)
+password_entry.grid(row=3, column=1)
+
+
+# Buttons
+generate_btn = Button(text="Generate Password", command=generate_password)
+generate_btn.grid(row=3, column=2)
+
+add_btn = Button(text="Add", width=44, command=add_data)
+add_btn.grid(row=4, column=1, columnspan=2)
+
+window.mainloop()
